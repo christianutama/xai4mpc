5f0169b7d116fa0b9e21fedcd18301ae34f18ab1
Christian Utama <christianutama@live.com>
2022-03-07 14:19:20 +0100

    main: add init offset, output calculation

--- main.py
+++ main.py
@@ -45,8 +45,8 @@ from template_simulator import template_simulator
 Get configured do-mpc modules:
 """
 model = template_model()
-mpc = template_mpc(model)
-simulator = template_simulator(model)
+mpc = template_mpc(model, 576)
+simulator = template_simulator(model, 576)
 estimator = do_mpc.estimator.StateFeedback(model)
 
 
@@ -57,6 +57,7 @@ Set initial state
 x0_min = np.array([[20.5], [18.0], [18.0], [ 5000.0]])
 x0_max = np.array([[22.5], [25.0], [25.0], [15000.0]])
 x0 = np.random.uniform(x0_min,x0_max) # Values between +3 and +3 for all states
+x0 = np.array([[20.5], [18.0], [18.0], [5000.0]]) # for benchmarking
 mpc.x0 = x0
 simulator.x0 = x0
 estimator.x0 = x0
@@ -74,16 +75,22 @@ plt.ion()
 """
 Run MPC main loop:
 """
-
-for k in range(7*24):
+u0_res = []
+for k in range(168):
     u0 = mpc.make_step(x0)
     y_next = simulator.make_step(u0)
     x0 = estimator.make_step(y_next)
+    u0_res.append(u0)
 
     graphics.plot_results(t_ind=k)
     graphics.plot_predictions(t_ind=k)
     graphics.reset_axes()
     plt.show()
     plt.pause(0.01)
+
+print(f"Total energy bought from/sold to the grid: {np.sum(graphics.data['_aux', 'P_grid'])/1000} kWh")
+print(f"Total heating energy: {np.sum(graphics.data['_u', 'P_heat'])/1000} kWh")
+u0_res = np.hstack(u0_res)
+# np.save('u0_mpc', u0_res)
 
 input('Press any key to exit.')
